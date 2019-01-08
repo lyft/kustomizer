b66ac246699db1fed60f8d2e99cdb0a8143bd575
Johnny Burns <jonathan.burns522@gmail.com>
2019-01-08 14:32:32 -0800

    updating the build script to support semver

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # kustomizer
-A container for running k8s kustomize.
+A container for running the k8s [kustomize](https://github.com/kubernetes-sigs/kustomize) tool.
 
 ## Usage
-mount a kustomizable directory to /kustomize and run kustomize within the image.
 
-example: `docker run -it -v "/path/to/local/dir":/kustomize lyft/kustomize:SHA kustomize build overlays/staging`
+mount a directory into the container at `/kustomize` and run kustomize within the image.
 
-## Updating
+example: `docker run -it -v /path/to/my/dir:/kustomize lyft/kustomize:v1.0.11 kustomize build overlays/staging`
+
+this will run the kustomization `/path/to/my/dir/overlays/staging/kustomization.yaml`
+
+The kustomizer docker images are tagged with the semver of the kustomize version used inside the image.
+
+## Developing this image
 
 To update the image to a new version of kustomize, change the version number specified in the Dockerfile, and update the sha256 value using the value from ``shasums.txt`` for your release: https://github.com/kubernetes-sigs/kustomize/releases
--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,14 @@
 set -e
 
+echo ""
+echo "------------------------------------"
+echo "          DOCKER BUILD"
+echo "------------------------------------"
+echo ""
+
+SHA=$(git rev-parse HEAD)
+RELEASE_SEMVER=$(git describe --tags --exact-match $GIT_SHA 2>/dev/null)
+
 if [ -n "$REGISTRY" ]; then
   # Do not push if there are unstaged git changes
   CHANGED=$(git status --porcelain)
@@ -10,32 +19,32 @@ if [ -n "$REGISTRY" ]; then
 fi
 
 docker build -t "$IMAGE_NAME:latest" .
+echo "$IMAGE_NAME:latest built locally."
+
 
 if [ -n "$REGISTRY" ]; then
-
-  SHA=$(git rev-parse HEAD)
-
-  IMAGE_TAG="${REGISTRY}/${IMAGE_NAME}:${SHA}"
-
-  docker tag "${IMAGE_NAME}:latest" "$IMAGE_TAG"
 
   if [ -n "${DOCKER_REGISTRY_PASSWORD}" ]; then
     docker login --username=${DOCKER_REGISTRY_USERNAME} --password=${DOCKER_REGISTRY_PASSWORD}
   fi
 
-  docker push "$IMAGE_TAG"
+  SHA_IMAGE_TAG="${REGISTRY}/${IMAGE_NAME}:${SHA}"
+
+  docker tag "${IMAGE_NAME}:latest" "$SHA_IMAGE_TAG"
+
+  docker push "$SHA_IMAGE_TAG"
+  echo "${SHA_IMAGE_TAG} pushed to remote"
+
+  if [ -n $RELEASE_SEMVER ]; then
+
+    SEMVER_IMAGE_TAG="${REGISTRY}/${IMAGE_NAME}:${RELEASE_SEMVER}"
+
+    docker tag "${IMAGE_NAME}:latest" "$SEMVER_IMAGE_TAG"
+    docker push "$SEMVER_IMAGE_TAG"
+    echo "${SEMVER_IMAGE_TAG} pushed to remote"
+  fi
 
 fi
 
 echo ""
-echo "------------------------------------"
-echo "              SUCCESS"
-echo "------------------------------------"
-echo ""
-
-echo "$IMAGE_NAME:latest built locally."
-
-if [ -n "$REGISTRY" ]; then
-  echo "${IMAGE_TAG} pushed to remote."
-fi
-echo ""
+echo "success"
